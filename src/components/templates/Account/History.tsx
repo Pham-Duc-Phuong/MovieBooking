c9f3f57f4e6c634c15a771706e930b8d23264e55
Pham Duc Phuong <phuong.mom33@gmail.com>
2023-10-04 20:43:07 +0700

    responsive history

--- src/components/templates/Account/History.tsx
+++ src/components/templates/Account/History.tsx
@@ -12,26 +12,26 @@ export const History = () => {
   const { UserThongTinDatVe } = useAuth()
   return (
     <div>
-      <div className="relative overflow-auto shadow-md sm:rounded-lg h-[750px] ">
-        <table className="w-full text-sm text-left text-gray-500">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+      <div className="overflow-auto shadow-md sm:rounded-lg h-[750px] ">
+        <table className="table-auto  w-full text-sm text-left text-gray-500">
+          <thead className="sm:text-xs phone:text-[10px] text-gray-700 uppercase bg-gray-50 ">
             <tr>
-              <th scope="col" className="px-6 py-3">
+              <th className="px-6 py-3">
                 Phim
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th className="px-6 py-3">
                 Mã vé
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th className="px-6 py-3">
                 Thời gian đặt
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th className="px-6 py-3">
                 Rạp
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th className="px-6 py-3">
                 Giá
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th className="px-6 py-3">
                 <i className="fa-regular fa-trash-can text-16 text-red-600"></i>
               </th>
             </tr>
@@ -42,15 +42,15 @@ export const History = () => {
                 return (
                   a?.danhSachGhe?.map((b, index) => {
                     return (
-                      <tr key={index} className="tr-table">
-                        <td scope="row" className="th-table flex items-center">
-                          <img className="w-[40px] mr-[10px]" src={a.hinhAnh} alt="" /><p className="text-gray-500 font-400">{a.tenPhim}</p>
+                      <tr key={index} className="tr-table sm:text-[14px] phone:text-[10px] items-center">
+                        <td scope="row" className="sm:flex sm:flex-row phone:flex-col items-center">
+                          <img className="w-[60px] mr-[10px]" src={a.hinhAnh} alt="" /><p className="text-gray-500 font-400">{a.tenPhim}</p>
                         </td>
                         <td className="px-6 py-4">
                           {b.maGhe}
                         </td>
                         <td className="px-6 py-4">
-                          <span className="mr-[15px]">{new Date(a.ngayDat).getHours()} : {new Date(a.ngayDat).getMinutes()}</span><span>{new Date(a.ngayDat).getDate()} / {new Date(a.ngayDat).getMonth()+1} / {new Date(a.ngayDat).getFullYear()}</span>
+                          <p className="mr-[15px]">{new Date(a.ngayDat).getHours()}:{new Date(a.ngayDat).getMinutes()}</p><p>{new Date(a.ngayDat).getDate()}/{new Date(a.ngayDat).getMonth()+1}/{new Date(a.ngayDat).getFullYear()}</p>
                         </td>
                         <td className="px-6 py-4">
                           {b.tenHeThongRap}
@@ -59,7 +59,7 @@ export const History = () => {
                           {a.giaVe}đ
                         </td>
                         <td className="px-6 py-4">
-                          <a href="#" onClick={() => {toast.success('Xóa thành công', {autoClose: 1000})}}><i className="fa-regular fa-trash-can text-16 text-red-200 cursor-pointer hover:text-red-600"></i></a>
+                          <a href="#" onClick={() => {toast.success('Xóa thành công', {autoClose: 1000})}}><i className="fa-regular fa-trash-can sm:text-16 phone:text-[14px] text-red-200 cursor-pointer hover:text-red-600"></i></a>
                         </td>
                       </tr>
                     )
